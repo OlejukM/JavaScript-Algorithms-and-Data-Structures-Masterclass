d021999c2a19fd2d5b80cd645bfb6887a766519b
Oleh Maslo <oleh.maslo@globallogic.com>
2020-09-10 16:40:15 +0300

    add radix sort.

--- script.js
+++ script.js
@@ -830,7 +830,7 @@
 //     let pivot = arr[start];
 //     let swapIndex = start;
 
-//     for (let i = start + 1; i < arr.length; i++) {
+//     for (let i = start + 1; i < end; i++) {
 //         if (pivot > arr[i]) {
 //             swapIndex++;
 //             swap(arr, swapIndex, i);
@@ -853,3 +853,38 @@
 //     return arr;
 // }
 // console.log(quickSort([4, 6, 9, 1, 2, 5, 3]));
+
+
+//--------------------------------------------------------------RADIX SORT------------------------------------------------------/
+
+// function getDigit(num, i) {
+//     return Math.floor(Math.abs(num) / Math.pow(10, i)) % 10;
+// }
+
+// function digitCount(num) {
+//     if (num === 0) return 1;
+//     return Math.floor(Math.log10(Math.abs(num))) + 1;
+// }
+
+// function mostDigits(nums) {
+//     let maxDigits = 0;
+//     for (let i = 0; i < nums.length; i++) {
+//         maxDigits = Math.max(maxDigits, digitCount(nums[i]));
+//     }
+//     return maxDigits;
+// }
+
+// function radixSort(nums) {
+//     const maxDigitCount = mostDigits(nums);
+
+//     for (let k = 0; k < maxDigitCount; k++) {
+//         let digitBuckets = Array.from({ length: 10 }, () => []);
+//         for (let i = 0; i < nums.length; i++) {
+//             let digit = getDigit(nums[i], k);
+//             digitBuckets[digit].push(nums[i]);
+//         }
+//         nums = [].concat(...digitBuckets);
+//     }
+//     return nums;
+// }
+// console.log(radixSort([23, 345, 5467, 12, 2345, 9852]));
